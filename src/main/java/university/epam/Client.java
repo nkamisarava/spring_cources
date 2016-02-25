2f3aa44511f5685158e1f28b2af1aab02311072c
nkamisarava <natallia_kamisarava@epam.com>
2016-02-25 15:02:11 +0300

    first

---
+++ src/main/java/university/epam/Client.java
@@ -0,0 +1,29 @@
+package university.epam;
+
+
+public class Client {
+
+    String id;
+    String fullName;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    Client(String id, String fullName) {
+        this.id = id;
+        this.fullName=fullName;
+    }
+}
---
+++ src/main/java/university/epam/ConsoleEventLogger.java
@@ -0,0 +1,9 @@
+package university.epam;
+
+
+public class ConsoleEventLogger {
+
+public void logEvent(String msg){
+    System.out.println(msg);
+}
+}
